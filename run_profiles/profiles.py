a29404a1bcb51bcfa175a7e4b91395564ffa7705
CSCFSG02\bbalushev <b_dimov@abv.bg>
2022-03-17 15:10:54 +0200

    add LCT profiles

--- run_profiles/profiles.py
+++ run_profiles/profiles.py
@@ -7,91 +7,7 @@ Created on Mon Jan 17 08:48:27 2022
 
 
 profile = {
-    'ExpressQA1': {
-        'Bebank': {
-            'URL': 'https://ui.qa1.bebank.ezbob.com/customer/wizard',
-            'connection_string': b'gAAAAABhA-UbPMZhyplqLcGAoVQs6KA7EYEiUTmaW1uWUpWWVNqIZnbyRfwb1QAmCLboeTavQoGZB'
-                                 b'-pE6rDgv2zMntIbttUBWIlLxSWSaU9Bt_H7AHKSaOwE6f3JwhDA1nczNuuNJJWOlRVkjyyCzDRMXGGmK'
-                                 b'_jmj6gZV2aPA-oLSau8xHO26i7zmaB8fqSOJxvVc38kgET32fxI_DAzDSTH2fjm-'
-                                 b'6lI3mKpeIvQChm1EZRMxANyUwRlt7j_HrYjpjn2WhX34kxj5lWG2-'
-                                 b'Q7ArD69bB3ICYhv6xk9YBv5kYOKrallY3hCuBzn5ST2Xp7aoyuS_pZlEqRUevCTavHVWE'
-                                 b'-urLqRqIsZnqPjcai58f96tL3qQhUWCs=',
-            'db': 'Ezbob',
-            'Login_URL': 'https://ui.qa1.bebank.ezbob.com/customer/login',
-            'UI_URL': 'ui.qa1.bebank.ezbob.com',
-            'API_URL': 'api2.qa1.express.ezbob.com',
-            'Swagger_URL': 'qa1.express-ops.ezbob.local',
-            'sync_api_host': 'service-data-sync-manager.qa1.express-ops.ezbob.local',
-            'sftp_host': '10.200.4.98',
-            'sftp_username': 'expusertpd',
-            'sftp_password': b'gAAAAABhA-IgnANlFWDsx2TNmryQzljF4d9pDGQkXuk_Wuo6wlbF1uiMc-'
-                             b'OyY0tkeDUkn7FC4rnkZkTPkjgApBy62RcAqFTJDA==',
-            'tenant_id': 'Bebank',
-            'Mailbox_URL': 'https://gmail.com/',
-            'Gmail_username': 'UIautomation@ezbobtest.com',
-            'Gmail_pass': b'gAAAAABhA-J2yd3UIrLW2K8Rxe_knEEIfIevhnyybfb_7CUlIXPJS6udaz2vI5_'
-                          b'SFGYSEhRc8BbBTtnv6uToHeqiNKsGiDQyWw==',
-            'Backoffice_URL': 'https://ezbcr1--qa1.lightning.force.com/',
-            'Backoffice_username': 'yarden.reitzes@ezbob.com.ezbcr1.qa1',
-            'Backoffice_pass': b'gAAAAABhA-KpT5SoyZmSc_dGayAHfBWzGbK53dZtuIplOCoXds2fLqu4nty0TefssahScbAU0-'
-                               b'nmkRJkRWV4Y6sS-1siPPEYfQ==',
-            'Backoffice_token': b'gAAAAABhA-L0CH5Eim4k2hwCkPnCTTc8olFDU-bQ3KaQOjF7yK0FWTRDlHG97RxWZV0H6vJa6Kk4'
-                                b'_FQWprs0UjVJHU-7D27sZWr4TYQuB4pZExt00Cb3qtI=',
-            'Tenant_authorization_token': 'Basic b25ib2FyZGluZ1VpOjBuYjA0cmQxbmdVMTVlY3JldA'
-        },
-        'Cebank': {
-            'URL': 'https://ui.qa1.cebank.ezbob.com/customer/wizard',
-            'connection_string': b'gAAAAABhA-UbPMZhyplqLcGAoVQs6KA7EYEiUTmaW1uWUpWWVNqIZnbyRfwb1QAmCLboeTavQoGZB'
-                                 b'-pE6rDgv2zMntIbttUBWIlLxSWSaU9Bt_H7AHKSaOwE6f3JwhDA1nczNuuNJJWOlRVkjyyCzDRMXGGmK'
-                                 b'_jmj6gZV2aPA-oLSau8xHO26i7zmaB8fqSOJxvVc38kgET32fxI_DAzDSTH2fjm-'
-                                 b'6lI3mKpeIvQChm1EZRMxANyUwRlt7j_HrYjpjn2WhX34kxj5lWG2-'
-                                 b'Q7ArD69bB3ICYhv6xk9YBv5kYOKrallY3hCuBzn5ST2Xp7aoyuS_pZlEqRUevCTavHVWE'
-                                 b'-urLqRqIsZnqPjcai58f96tL3qQhUWCs=',
-            'db': 'Cebank',
-            'Login_URL': 'https://ui.qa1.cebank.ezbob.com/customer/login',
-            'UI_URL': 'ui.qa1.cebank.ezbob.com',
-            'API_URL': 'api2.qa1.express.ezbob.com',
-            'Swagger_URL': 'qa1.express-ops.ezbob.local',
-            'sync_api_host': 'service-data-sync-manager.qa1.express-ops.ezbob.local',
-            'sf_api_host': 'service-clcm-ezbob-express.qa1.express-ops.ezbob.local',
-            'sf_client_id': b'gAAAAABhbr0JspAw9XWMMlfakzrjk4-d9NTY0S-KKMyLSK8utigfPcnRtm2TLRuJ0rl71QuoEDOHqMIuT7N8Y00r'
-                            b'csDZyOUVaw==',
-            'sf_client_secret': b'gAAAAABhbr0JvpfPpjgxv85wxm17y88EnFk_6rnWaPGG3OZcXJbI9iVZKciB0lspC7dp1mnv_QJ_XvbVkOz'
-                                b'iTHxJCkia4CCKT284mqd2VZTxIOW_xoMzOSM=',
-            'sftp_host': '10.200.4.98',
-            'sftp_username': 'expusertpd',
-            'sftp_password': b'gAAAAABhA-IgnANlFWDsx2TNmryQzljF4d9pDGQkXuk_Wuo6wlbF1uiMc-'
-                             b'OyY0tkeDUkn7FC4rnkZkTPkjgApBy62RcAqFTJDA==',
-            'tenant_id': 'Cebank',
-            'Mailbox_URL': 'https://gmail.com/',
-            'Gmail_username': 'UIautomation@ezbobtest.com',
-            'Gmail_pass': b'gAAAAABhA-J2yd3UIrLW2K8Rxe_knEEIfIevhnyybfb_7CUlIXPJS6udaz2vI5_'
-                          b'SFGYSEhRc8BbBTtnv6uToHeqiNKsGiDQyWw==',
-            'Backoffice_URL': 'https://ezbcr1--qa1.lightning.force.com/',
-            'Backoffice_username': 'yarden.reitzes@ezbob.com.ezbcr1.qa1',
-            'Backoffice_pass': b'gAAAAABhA-KpT5SoyZmSc_dGayAHfBWzGbK53dZtuIplOCoXds2fLqu4nty0TefssahScbAU0-'
-                               b'nmkRJkRWV4Y6sS-1siPPEYfQ==',
-            'Backoffice_token': b'gAAAAABhA-L0CH5Eim4k2hwCkPnCTTc8olFDU-bQ3KaQOjF7yK0FWTRDlHG97RxWZV0H6vJa6Kk4'
-                                b'_FQWprs0UjVJHU-7D27sZWr4TYQuB4pZExt00Cb3qtI=',
-            'Tenant_authorization_token': 'Basic b25ib2FyZGluZ0NlYmFua1VpOjNpT1o6d3FsdnltSWRFXyQ',
-            'SF_authorization_token': 'Basic Q2ViYW5rQ1JNOjwhdVJhQjYsd3w9MG9tLXU='
-        }
-    },
-    'TestEnvironment': {
-        'default': {
-            'URL': 'http://motor.argenta.project.s3-website-eu-west-1.amazonaws.com/omnichannel/standard-launcher/#/',
-            'UI_URL': 'http://motor.argenta.project.s3-website-eu-west-1.amazonaws.com/omnichannel/standard-launcher/#/',
-            'API_URL': 'api2.qa5.metro.ezbob.com',
-            'connection_string': b'gAAAAABhA-cJ4PMNPDU97eRRYJ-V4-Q6WDrGcaV5s_dS7TU1TwOv7v6ef8F4DYYwGZzklcKNIIbrV4_KzyerxQ6L'
-                                 b'gA8x_D7m7z7taqGeNfD9OFxRjsYXVXy3oUDWgjXDaPz5_Gf4HjnEsnhfDMzSGKOkC0NPPaIafjyWX9Q5CTCIncH3'
-                                 b'CsKGk3z7xXOgtWCxyKs_90EYdP4au6HkAQu5LioVPFvu9kUrNcl7WI0iAaqbBI512tXh697YOKOGxhdiggFYus'
-                                 b'CCEhPnlLfiRObu8z180QnfykBT4mi-XHZsbH8y9mlSf79EUSsTBOtXoN5LhjDgaBq9tv98NCW5ptyyP4O9wqyLKw'
-                                 b'6PHZ6_B-ZYUbvvKjc2BM1tdYE=',
-            'password': b'gAAAAABhA-O9TPV8lylhOFjHqTInG4Z3k5ttjFOmWUR7zTPGgB0oyITuVOl41bmm-'
-                        b'RvgJsBi2_HqVBw1HIQOvEZTgBCeJNe8JA=='
-        }
-    },
-    'ArgentaRunLCT_1': {
+    'LCT_1': {
         'default': {
             'UI_URL': 'http://motor.argenta.project.s3-website-eu-west-1.amazonaws.com/omnichannel/standard-launcher/#/',
             'API_URL': 'http://20.33.40.95:13211/csc/insurance',
@@ -101,9 +17,9 @@ profile = {
             'login_password': b'gAAAAABh6RDn4K8Uty8g3UiaSQkTKlHCq6Jxt6IOZsPoqywuE9jKPo9BUVLGl_pfR9fd_E_XalTRIG7pRiiw2zNqjpAaarOj2A=='
         }
     },
-    'ArgentaInsurance_2': {
+    'LCT_2': {
         'default': {
-            'UI_URL': 'http://motor.argenta.project.s3-website-eu-west-1.amazonaws.com/omnichannel/standard-launcher/#/',
+            'UI_URL': 'https://www.lct.argenta-dxc-graphtalk-insurance.com/',
             'API_URL': 'http://20.33.40.95:13213/csc/insurance',
             'auth_username': b'gAAAAABiHy-nH0uX1qLX4cvXmchaOUlQNYZIe6G1h4zsIfL6J0YGPQ_Wacv90yM5lXy2Uvy7S7KHkdTpdaZ1aA4lYU04j_fo8g==',
             'auth_key': b'gAAAAABiHy-nzlnsJSHrNnkF6dxiYfqF_dBuu2SwazzZAsfa_YQZrN3t8TPHLcKeBA6U6AGa6E-fYJi7T84by1Sr05IYJK_gozaV4DR9KgCFTcgwdA-npTPSIdiOV_aXP03-4QM7uvDI',
